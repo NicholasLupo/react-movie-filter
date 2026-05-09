f3a80fe7a17cf26e3bf95425e6c7e4def47259bf
NicholasLupo <nicholaslupo14@gmail.com>
2026-05-09 12:38:38 +0200

    add select and id

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 function App() {
 
   const films = [
-    { title: 'Inception', genre: 'Fantascienza' },
-    { title: 'Il Padrino', genre: 'Thriller' },
-    { title: 'Titanic', genre: 'Romantico' },
-    { title: 'Batman', genre: 'Azione' },
-    { title: 'Interstellar', genre: 'Fantascienza' },
-    { title: 'Pulp Fiction', genre: 'Thriller' },
+    { id: 1, title: 'Inception', genre: 'Fantascienza' },
+    { id: 2, title: 'Il Padrino', genre: 'Thriller' },
+    { id: 3, title: 'Titanic', genre: 'Romantico' },
+    { id: 4, title: 'Batman', genre: 'Azione' },
+    { id: 5, title: 'Interstellar', genre: 'Fantascienza' },
+    { id: 6, title: 'Pulp Fiction', genre: 'Thriller' },
   ]
-
 
   return (
     <>
       <main>
-        Ciao
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+        <ul>
+
+        </ul>
       </main>
     </>
   )

ff00f9f871ade5f313460970613d24f2094e9ffd
NicholasLupo <nicholaslupo14@gmail.com>
2026-05-09 12:35:19 +0200

    add object

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 function App() {
 
+  const films = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
+
+
   return (
     <>
-
+      <main>
+        Ciao
+      </main>
     </>
   )
 }
